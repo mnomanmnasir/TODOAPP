92733f6fb0fe8910278a3bf9d85be1fde9e43f55
Noman Muhammad Nasir <102538287+mnomanmnasir@users.noreply.github.com>
2022-12-19 09:44:18 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,73 +1,63 @@
+var el = document.getElementById('result');
+var names = [];
 
-var list = document.getElementById('list');
+function Create() {
+    var el = document.getElementById('name')
+	var name = el.value;
+	if (name) {
+		names.push(name.trim());
+		el.value = '';
+		displayData();
+	}
+	displayData();
+};
 
+function Delete(item) {
+	names.splice(item, 1);
+	displayData();
+	
+};
 
-function addTodo()
-
-{
-    var todo_item = document.getElementById('todoitem')
-
-    
-    var li = document.createElement("li");
-    var liText = document.createTextNode(todo_item.value);
-    li.appendChild(liText);   
-    
-
-    var delBtn = document.createElement("button")
-
-    var delText = document.createTextNode("DELETE")
-
-    delBtn.setAttribute("class","Btn")
-    delBtn.setAttribute("onclick","deleteItem(this)") //this pora button console pr dyga 
-    delBtn.appendChild(delText)
-
-
-    //create edit button
-
-    var editBtn = document.createElement("button")
-    var editText = document.createTextNode("EDIT")
-
-    
-    editBtn.setAttribute("class","Btn")
-    editBtn.appendChild(editText)
-    editBtn.setAttribute("onclick","editItem(this)")
-
-    li.appendChild(editBtn)
-
-
-    
-    li.appendChild(delBtn)
+function Edit(item) {
+	var el = document.getElementById('update_name');
+	el.value = names[item];
+	document.getElementById('edit').style.display = 'inline';
+	document.getElementById('create').style.display = 'none';
+	self = this;
+	
+	document.getElementById('update').onsubmit = function() {
+	var name = el.value;
+		if (name) {
+			self.names.splice(item, 1, name.trim());
+			self.displayData();
+			buttonToggle();
+			document.getElementById('create').style.display = 'inline';
+		}
+	}
+};
 
 
-
-    // console.log(li)
-
-    todo_item.value = " "
-    list.appendChild(li)
-    // console.log(todo_item.value)
+function buttonToggle() {
+	document.getElementById('edit').style.display = 'none';
+	document.getElementById('create').style.display = 'inline';
 }
 
 
-function deleteItem(e){
+function displayData() {
+	var data = '';
+	if (names.length > 0) {
+		for (i = 0; i < names.length; i++) {
+			data += '<tr>';
+			data += '<td>' + names[i] + '</td>';
+			data += '<td colspan="2"><center><button class="btn btn-warning" onclick="Edit(' + i + ')"><span class="glyphicon glyphicon-edit"></span> Edit</button> | <button class="btn btn-danger" onclick="Delete(' + i + ')"><span class="glyphicon glyphicon-trash"></span> Delete</button></center></td>';
+			data += '</tr>';
+		}
+	}
+	
+	el.innerHTML = data;
+};
 
-    
-    e.parentNode.remove(); // ye function remove krdeyga item
 
-}
-
-
-function deleteall(){
-    
-    list.innerHTML = " "
-}
-
-
-
-
-function editItem(e){
-
-    var editValue = prompt("Enter edit value",e.parentNode.firstChild.nodeValue);
-    // console.log(e.parentNode.firstChild)
-
-    e.parentNode.firstChild.nodeValue = editValue
-}
+displayData();
+buttonToggle();
+	
